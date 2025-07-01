2e8d705fdfffe8e09965c0e266af8af5b44855b1
He-llo-Wo-rld <serhiiforwork@gmail.com>
2025-07-01 17:42:57 +0100

    fix

--- cleanup-unused-files.sh
+++
@@ -1,5 +0,0 @@
-# Delete unused SearchBar-new.tsx file
-rm components/features/search/SearchBar-new.tsx
-
-# Or if you want to keep it as backup, rename it
-mv components/features/search/SearchBar-new.tsx components/features/search/SearchBar-backup.tsx
